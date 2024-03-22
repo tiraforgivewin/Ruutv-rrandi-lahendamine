bac124f285110356d1d38224fbb0af531eb4cd4e
opilane <opilane@HP-CZC2349HV2>
2024-03-22 11:29:50 +0200

    Add project files.

---
+++ Ruutvõrrandi_lahendamine.py
@@ -0,0 +1,29 @@
+﻿from tkinter import *
+
+
+
+
+
+
+aken=Tk()
+aken.geometry("800x400")
+aken.title("Tkineri kasutamine")
+
+tekst1="Ruutvõrrandi lahendamine"
+
+pealkiri=Label(aken,
+               text=tekst1,
+               bg="#bddbf0",
+               fg="#04951d",
+               font="Arial 20",
+               height=3,
+               width=len(tekst1))
+esForm=Label(aken,
+             )
+
+
+
+obj=[pealkiri]
+for i in obj:
+    i.pack()
+aken.mainloop()
